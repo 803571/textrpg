79fb78ae1189f96124f53902551c09537146d2fc
803571 <8omsut@naver.com>
2024-08-24 03:57:53 +0900

    추가: 몬스터 처치 시 문구 출력, 로딩 시 문구 추가, 도주하기 기능 추가

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 import chalk from "chalk";
 import readlineSync from "readline-sync";
 
+// 이것은, 어렸을 때 하던 파이널판타지가 될 수도 있고, 쯔꾸르가 될 수도 있다.
+// 근데 파판은 안해봄 ㅋㅋ
+
+// ====================================== 이곳은 플레이어 스펙, 몬스터 스펙을 보는 구간입니다 ======================================
 class Player {
   constructor() {
-    this.hp = 100;
+    this.hp = 100; // 플레이어 피통
+    this.attackPower = 15; // 플레이어 기본데미지
   }
 
-  attack() {
-    // 플레이어의 공격
+  attack(monster) {
+    // 플레이어 -> 몬스터에게 공격
+    let damage = this.attackPower;
+    monster.hp -= damage;
+    return damage;
   }
 }
 
@@ -21,18 +29,31 @@ class Monster {
   }
 }
 
+// ====================================== 이곳은 플레이어 스펙, 몬스터 스펙을 보는 구간입니다 ======================================
+
+// ====================================== 게임 시작 로비화면 ======================================
+
 function displayStatus(stage, player, monster) {
-  console.log(chalk.magentaBright(`\n=== Current Status ===`));
+  // console.log(chalk.magentaBright(`\n=== Current Status ===`));
+  console.log(chalk.magentaBright(`\n===== 고요한 숲 =====`));
   console.log(
+    // chalk.cyanBright(`| Stage: ${stage} `) +
+    //   chalk.blueBright(`| 플레이어 정보`) +
+    //   chalk.redBright(`| 몬스터 정보 |`)  // 기본 설정입니다. 수정한 코드가 문제없이 작동한다면 삭제하세요.
     chalk.cyanBright(`| Stage: ${stage} `) +
-      chalk.blueBright(`| 플레이어 정보`) +
-      chalk.redBright(`| 몬스터 정보 |`)
+      chalk.blueBright(`| 용사 HP: ${player.hp} V`) +
+      chalk.redBright(`S 몬스터 HP: ${monster.hp} |`)
   );
   console.log(chalk.magentaBright(`=====================\n`));
 }
 
+// ====================================== 게임 시작 로비화면 ======================================
+
+// ====================================== 전투 시스템 ======================================
+
 const battle = async (stage, player, monster) => {
-  let logs = [];
+  // 전투 시스템에 필요한 매개변수를 받아온다! 전투를 구성하는데 필요한 건 스테이지, 전투에 참여하는 플레이어와 몬스터
+  let logs = []; // 전투가 진행되는 모든 메시지를 담는 함.
 
   while (player.hp > 0) {
     console.clear();
@@ -40,13 +61,67 @@ const battle = async (stage, player, monster) => {
 
     logs.forEach((log) => console.log(log));
 
-    console.log(chalk.green(`\n1. 공격한다 2. 아무것도 하지않는다.`));
-    const choice = readlineSync.question("당신의 선택은? ");
+    console.log(chalk.green(`\n1. 기본공격 \n2. 스킬 \n3. 아무것도 하지 않는다 \n4. 도주하기`)); // 플레이어한테 선택지를 줌
+    const choice = readlineSync.question("용사는 무얼 할까?? "); // 단순 나레이션
 
-    // 플레이어의 선택에 따라 다음 행동 처리
-    logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+    if (choice === "1") {
+      //플레이어가 공격한다를 골랐을 경우
+      let damage = player.attack(monster);
+      logs.push(
+        chalk.green(`용사가 몬스터에게 `) +
+          chalk.yellow(`${damage}`) +
+          chalk.green(` 만큼 피해를 주었습니다.`)
+      );
+
+      if (monster.hp <= 0) {
+        console.clear();
+        displayStatus(stage, player, monster); // 몹잡고 최종 상태 불러오기
+        logs.push(chalk.green(`  << 몬스터를 처치했습니다! >> `));
+        logs.forEach((log) => console.log(log));
+
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 대기
+
+        console.clear();
+        console.log(
+          chalk.cyanBright(`용사는 가던 길을 마저 걷기 시작했습니다.. `)
+        );
+
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 대기
+
+        console.clear();
+        console.log(chalk.cyanBright(`앗! 야생의 몬스터가 나타났다! `));
+
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 대기
+
+        // break;
+        return;
+      }
+
+      // 추가? 몬스터가 반격
+      // 추가? 몬스터도 플레이어에게 공격
+    } else if (choice === "3") {
+      logs.push(chalk.yellow(`용사는 아무것도 하지 않았습니다. `));
+    } else if (choice === "4") {
+      let escapeChance = Math.random();
+      if (escapeChance <= 0.3) {
+        console.clear();
+        console.log(chalk.green(`성공적으로 도주했다!!`));
+
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 대기
+
+        return 'escape';
+      } else {
+        logs.push(chalk.red(`이런, 도주에 실패했다!`));
+      }
+    } else {
+      logs.push(
+        chalk.red(`올바른 선택을 하시게나, 젊은이.  ... '어디선가 이상한 소리가 들리는 듯 하다.'`));
+    }
   }
+  logs.forEach((log) => console.log(log)); // 전투가 끝났을 때 로그
 };
+
+// ====================================== 전투 시스템 ======================================
 
 export async function startGame() {
   console.clear();
@@ -54,8 +129,22 @@ export async function startGame() {
   let stage = 1;
 
   while (stage <= 10) {
-    const monster = new Monster(stage);
-    await battle(stage, player, monster);
+    let monster = new Monster(stage);
+    let battleresult;
+
+    do {
+      battleresult = await battle(stage, player, monster); // battle 함수에서 도주 성공시 escape를 반환하면, battle 함수가 종료되고 escape를 여기서 호출자(startgame)에게 전달, battleresult변수에 저장
+      if (battleresult === 'escape') {
+        console.log(chalk.cyanBright(`현재 스테이지에서 전투를 재시작합니다. `));
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 대기        
+        monster = new Monster(stage); // 도주 후 몹 다시 젠
+      }
+    } while (battleresult === 'escape'); // 도주 성공 시 전투를 재시작
+
+    if (player.hp <= 0) {
+      console.log(chalk.red(`You Died.`));
+      break;
+    }
 
     // 스테이지 클리어 및 게임 종료 조건
 
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function displayLobby() {
     // 타이틀 텍스트
     console.log(
         chalk.cyan(
-            figlet.textSync('RL- Javascript', {
+            figlet.textSync('TexT - RPG', {
                 font: 'Standard',
                 horizontalLayout: 'default',
                 verticalLayout: 'default'
@@ -23,15 +23,15 @@ function displayLobby() {
     console.log(line);
 
     // 게임 이름
-    console.log(chalk.yellowBright.bold('CLI 게임에 오신것을 환영합니다!'));
+    console.log(chalk.yellowBright.bold('TexTRPG에 오신것을 환영합니다!'));
 
     // 설명 텍스트
-    console.log(chalk.green('옵션을 선택해주세요.'));
+    console.log(chalk.green('Ver. 0.1'));
     console.log();
 
     // 옵션들
-    console.log(chalk.blue('1.') + chalk.white(' 새로운 게임 시작'));
-    console.log(chalk.blue('2.') + chalk.white(' 업적 확인하기'));
+    console.log(chalk.blue('1.') + chalk.white(' 새로운 여정'));
+    console.log(chalk.blue('2.') + chalk.white(' 업적 열람'));
     console.log(chalk.blue('3.') + chalk.white(' 옵션'));
     console.log(chalk.blue('4.') + chalk.white(' 종료'));
 
@@ -43,22 +43,42 @@ function displayLobby() {
 }
 
 // 유저 입력을 받아 처리하는 함수
-function handleUserInput() {
+async function handleUserInput() {
     const choice = readlineSync.question('입력: ');
 
     switch (choice) {
         case '1':
             console.log(chalk.green('게임을 시작합니다.'));
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+
+            console.log(chalk.green('개발자가 맵을 갈고 닦는 중..'));
+            console.log(chalk.green('■■■■□□□□□□ 40%'));
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+
+            console.clear();
+            console.log(chalk.green('집 나간 고블린 데리고 오는 중..'));
+            console.log(chalk.green('■■■■■■□□□□ 60%'));
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+
+            console.clear();
+            console.log(chalk.green('항해를 떠나기 위해 닻을 올리는 중..'));
+            console.log(chalk.green('■■■■■■■■■□ 90%'));
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+
+            console.clear();
+            console.log(chalk.green('용사가 생성되었습니다.'));
+            console.log(chalk.green('■■■■■■■■■■ 100%'));
+            await new Promise((resolve) => setTimeout(resolve, 2000));
             // 여기에서 새로운 게임 시작 로직을 구현
             startGame();
             break;
         case '2':
-            console.log(chalk.yellow('구현 준비중입니다.. 게임을 시작하세요'));
+            console.log(chalk.yellow('이제 모험을 떠나는 풋내기 애송이가 업적이 어디있는가. 게임을 시작하세요.'));
             // 업적 확인하기 로직을 구현
             handleUserInput();
             break;
         case '3':
-            console.log(chalk.blue('구현 준비중입니다.. 게임을 시작하세요'));
+            console.log(chalk.blue('도구함을 잠근 자물쇠의 비밀번호가 기억나지 않습니다.. 게임을 시작하세요.'));
             // 옵션 메뉴 로직을 구현
             handleUserInput();
             break;
@@ -68,7 +88,7 @@ function handleUserInput() {
             process.exit(0); // 게임 종료
             break;
         default:
-            console.log(chalk.red('올바른 선택을 하세요.'));
+            console.log(chalk.red('올바른 선택을 하시게나, 젊은이.'));
             handleUserInput(); // 유효하지 않은 입력일 경우 다시 입력 받음
     }
 }
